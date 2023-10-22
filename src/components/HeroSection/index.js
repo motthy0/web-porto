b173d3707dde69eed10e56b3bf51495a9f8c7e6e
motthy0 <sanscoding0@gmail.com>
2023-10-22 23:40:31 +0700

    initial

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { HeroContainer, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle } from './HeroStyle'
+import Hero from '../../images/Hero.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={Hero} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
